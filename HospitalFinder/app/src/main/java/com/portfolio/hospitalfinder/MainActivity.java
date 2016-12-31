08ebecce7bd15a9342aa1f8a6b12e0d63d443b8e
Rio <18drumer@naver.com>
2016-12-31 10:54:53 +0900

    네이버 Map Activity 연동 추가

--- HospitalFinder/app/src/main/java/com/portfolio/hospitalfinder/MainActivity.java
+++ HospitalFinder/app/src/main/java/com/portfolio/hospitalfinder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.portfolio.hospitalfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.portfolio.hospitalfinder.naver.HospitalMapActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,8 +30,10 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, HospitalMapActivity.class);
+                startActivity(intent);
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
 
---
+++ HospitalFinder/app/src/main/java/com/portfolio/hospitalfinder/naver/HospitalMapActivity.java
@@ -0,0 +1,31 @@
+package com.portfolio.hospitalfinder.naver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.nhn.android.maps.NMapActivity;
+import com.nhn.android.maps.NMapView;
+
+public class HospitalMapActivity extends NMapActivity  {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_hospital_map);
+//    }
+
+    private NMapView mMapView;// 지도 화면 View
+    private final String CLIENT_ID = "GAbanOHF8FGXvDJN1v6s";// 애플리케이션 클라이언트 아이디 값
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mMapView = new NMapView(this);
+        setContentView(mMapView);
+        mMapView.setClientId(CLIENT_ID); // 클라이언트 아이디 값 설정
+        mMapView.setClickable(true);
+        mMapView.setEnabled(true);
+        mMapView.setFocusable(true);
+        mMapView.setFocusableInTouchMode(true);
+        mMapView.requestFocus();
+    }
+}
